e5fc1b5af175fe6f3b7326312ab448b3f5f2c795
DaianeSouza10 <44443749+DaianeSouza10@users.noreply.github.com>
2020-02-10 20:17:09 -0300

    Adição das configurações iniciais referente ao servidor

--- app.js
+++ app.js
@@ -1,17 +1,40 @@
 const express = require('express');
 const app = express();
 
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const url = 'mongodb+srv://usuario_admin:_nivy2020@cluster0-jn8ue.mongodb.net/test?retryWrites=true&w=majority'; //STRING DE CONEXÃO
+const options = { reconnectTries:Number.MAX_VALUE, reconnectInterval: 500, poolSize: 5, useNewUrlParser: true};
+//useUnifiedTopology: true
+
+mongoose.connect(url,options);
+//mongoose.set('useCreateIndex', true);
+
+/*
+mongoose.connection.on('error', (err) => {
+    console.log('Erro na conexão com o banco de dados: ' + err);
+})
+
+mongoose.connection.on('desconected', () =>{
+    console.log('Aplicação desconectada!');
+})
+
+mongoose.connection.on('connected', () =>{
+    console.log('Aplicação conectada!!');
+})*/
+
+//BODY PARSER
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 const indexRoute = require('./Routes/index');
- 
+const userRoute = require('./Routes/users');
 
-app.get('/', (req,res) => {
-    return res.send({message: 'tudo ok com o metodo GET!'})
+app.use('/',indexRoute);
+app.use('/users', userRoute);
+
+app.listen(3000,()=>{
+    console.log("Servidor funcionando...")
 });
-
-app.post('/', (req,res) => {
-    return res.send({message: 'tudo ok com o metodo POST!'})
-});
-
-app.listen(3000);
 
 module.exports = app;
